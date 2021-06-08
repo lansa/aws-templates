e1b59ad1daf54da2cd07e52de2a14d946e8ac9c7
Sharmil <skumar@gomoxie.com>
2021-06-08 14:54:02 +0530

    163405 Updated region parameter and removed Cookbooksbranchs parameter and it's references

--- scripts/AlternateImageVersion.ps1
+++ scripts/AlternateImageVersion.ps1
@@ -9,20 +9,19 @@ param (
 
     [Parameter(Mandatory=$true)]
     [string]
-    $CookbooksBranch
+    $region
 )
 
 $SkuName = "$($Gateversion)"
 Write-Host "$SkuName"
 # Get the URL from the stack
-$output = (Get-CFNStack -StackName $($Gatestack) -region ap-southeast-2).Outputs
+$output = (Get-CFNStack -StackName $($Gatestack) -region $($region)).Outputs
 $websiteUrl = $output | Where-Object {$_.OutputKey -eq "WebsiteURL"}
 $url = $websiteUrl.OutputValue
-$CookbooksBranch = "$($CookbooksBranch)"
 
 # Autoscaling Instance Id
 $childstack = (Get-CFNStack | Where-Object {$_.StackName -match "$($Gatestack)-Web" }).StackName
-$webServerGroupResource = (Get-CFNStackResource -StackName $childstack -region ap-southeast-2 -logicalResourceId WebServerGroup)
+$webServerGroupResource = (Get-CFNStackResource -StackName $childstack -region $($region) -logicalResourceId WebServerGroup)
 $instanceDetails = Get-ASAutoScalingInstance | ? {$_.AutoScalingGroupName -eq $webServerGroupResource.PhysicalResourceId} | select -ExpandProperty InstanceId | Get-EC2Instance | select -ExpandProperty RunningInstance | ft InstanceId, PrivateIpAddress
 $instanceId = (Get-ASAutoScalingGroup -AutoScalingGroupName $webServerGroupResource.PhysicalResourceId).Instances.InstanceId
 
