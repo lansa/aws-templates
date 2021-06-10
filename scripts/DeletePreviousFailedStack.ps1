1df0db7cbaf86900035d4bf4942edf9882f30c79
Sharmil <skumar@gomoxie.com>
2021-06-10 10:58:45 +0530

    163405 Added Gatestack parameter in the script

--- scripts/DeletePreviousFailedStack.ps1
+++ scripts/DeletePreviousFailedStack.ps1
@@ -1,10 +1,16 @@
+param (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $Gatestack
+)
+
 try{
-Get-CFNStack -StackName $(Gate.stack)
-Remove-CFNStack -StackName $(Gate.stack) -Force
+Get-CFNStack -StackName $($Gatestack)
+Remove-CFNStack -StackName $($Gatestack) -Force
 $count =0
 while($count -lt 20){
    try{
-         Get-CFNStack -StackName $(Gate.stack)
+         Get-CFNStack -StackName $($Gatestack)
          Start-Sleep -Seconds 200
          Write-Host "Deleting stack"
          $count = $count +1
