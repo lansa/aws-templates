f1abee8a71f5a697cdbcda36a7510112c7e01c7f
Rizwan Ansari <rizwan@celestialsys.com>
2021-06-11 20:06:12 +0530

    163405 Add changes for updating template Json file.
    I have added the code which we discussed to update Template
    JSON file with ami IDs.
    
    Still there is one issues while updating template JSOn file, It is adding '@'
    in the begining of line, which need to be debug and fixed.

--- scripts/UpdateTemplateAMIMappings.ps1
+++ scripts/UpdateTemplateAMIMappings.ps1
@@ -1,18 +1,13 @@
 param (
     [Parameter(Mandatory=$true)]
     [string]
-    [ValidateNotNull()]
-    $TemplateUrl,
-
-    [Parameter(Mandatory=$true)]
-    [string]
+    [ValidateSet("Production", "Development")]
     $ImageType
-)
+    )
 
-Write-Host "TemplateURL: $TemplateUrl and ImageType: $ImageType"
-# Ensures that Invoke-WebRequest uses TLS 1.2
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$TemplateJson = Invoke-WebRequest $TemplateUrl | ConvertFrom-Json
+$FilePath = "$($env:System_DefaultWorkingDirectory)\_lansa_aws-templates\CloudFormationWindows\lansa-master-win.cfn.template"
+
+$TemplateJson = Get-Content -Path  $FilePath | ConvertFrom-Json
 
 $BaseImageNameArray = @(
     'w12r2d-14-2'
@@ -21,33 +16,80 @@ $BaseImageNameArray = @(
     'w16d-15-0'
     'w19d-14-2'
     'w19d-15-0'
+    'w16d-14-2j'
+    'w16d-15-0j'
+    'w19d-14-2j'
+    'w19d-15-0j'
 )
+
 if ( $TemplateJson ) {
 
     Write-Host("JSON loaded" )
 
-    #$TemplateJson
     $AMI142 = $TemplateJson.Mappings.AWSRegionArch2AMI142
-    Write-Host( "V14 SP2 AMIs")
-    $AMI142 | Out-Default | Write-Host
+    Write-Host "V14 SP2 AMIs $AMI142"
 
     $AMI15 = $TemplateJson.Mappings.AWSRegionArch2AMI15
-    Write-Host( "V15 GA AMIs")
-    $AMI15 | Out-Default | Write-Host
+    Write-Host "V15 GA AMIs $AMI15"
 
+    $AMIList = @()
     foreach ( $ImageName in $BaseImageNameArray ) {
-        Clear-Variable -name path
-        if ( $ImageType -eq "Development" ) {
-            $path = "$($env:System_DefaultWorkingDirectory)/_Build Image Release Artefacts/aws-$ImageName/$ImageName.txt"
-            $region = "ap-southeast-2"
-        } elseif ( $ImageType -eq "Production" ) {
-            $path = "$($env:System_DefaultWorkingDirectory)/copy-$ImageName.txt"
-            $region = "us-east-1"
-        } else {
-            Write-Host "ImageType $ImageType is not supported"
+        switch ($ImageType)
+        {
+            Development
+            {
+                $path = "$($env:System_DefaultWorkingDirectory)/_Build Image Release Artefacts/aws-$ImageName/$ImageName.txt"
+                $Region = 'ap-southeast-2'
+            }
+            Production
+            {
+                $path = "$($env:System_DefaultWorkingDirectory)/copy-$ImageName.txt"
+                $Region = 'us-east-1'
+            }
         }
+
         $amiID = Get-Content -Path $path
-        
-        #Update Template file based on amiID read from the file above.
+        if ( $amiID ) {
+            $AMIList += $amiID
+        } else {
+            $AMIList += "skip"
+        }
+        Clear-Variable -name path
+        Clear-Variable -name amiID
     }
+
+    $index = 0
+    foreach ($win in @("win2012", "win2016", "win2019", "win2016jpn", "win2019jpn")) {
+        # Check whether an AMI exists in the template for this Region/Win version
+        if ( Get-Member -inputobject $AMI142 -name "$Region" ) {
+            if ( Get-Member -inputobject $AMI142.$Region -name "$win" ) {
+                if ( $AMIList[$index] -ne "skip") {
+
+                    $TemplateJson.Mappings.AWSRegionArch2AMI142.$Region.$win = $AMIList[$index]
+                    $index++
+                }
+            } else {
+                Write-Host "$win V14.2 There is no AMI for $win"
+            }
+        } else {
+            Write-Host "$win V14.2 There is no AMI in $Region"
+        }
+
+        if ( Get-Member -inputobject $AMI15 -name "$Region" ) {
+            if ( Get-Member -inputobject $AMI15.$Region -name "$win") {
+                if ( $AMIList[$index] -ne "skip") {
+                    $TemplateJson.Mappings.AWSRegionArch2AMI15.$Region.$win = $AMIList[$index]
+                    $index++
+                }
+            } else {
+                Write-Host "$win V15 There is no AMI for $win"
+            }
+        } else {
+            Write-Host "$win V15 There is no AMI in $Region"
+        }
+    }
+    $TemplateJson  | ConvertTo-Json | set-content $FilePath
+
+} else {
+    Throw "Template file $FilePath does not exist"
 }
