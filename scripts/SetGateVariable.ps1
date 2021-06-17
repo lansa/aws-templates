ff46a19f850a101f635fe7716436849ef739305a
Sharmil <skumar@gomoxie.com>
2021-06-17 12:52:56 +0530

    Revert "163405 Removing the task variable"
    This reverts commit a3be88f434e8acf1119af5d9ca4eea2674ae911b.

--- scripts/SetGateVariable.ps1
+++ scripts/SetGateVariable.ps1
@@ -14,20 +14,25 @@ Write-Host "##vso[task.setvariable variable=IsEnabled;isOutput=true]False"
 $path = "$($env:System_DefaultWorkingDirectory)/_Build Image Release Artefacts/aws-$BaseImageName/$BaseImageName.txt"
 Write-Host "Using $path"
 if (Test-Path $path) {
-    $amiID = Get-Content -Path $path
-    Write-Host "AMI ID $($amiID)"
-    $imageName = (Get-EC2Image -ImageId $amiID).Name
-    $imageName -match "$BaseImageName[-]?[0-9]+"
-    $version = $Matches[0]
-    Write-Host "Version : $version"
-    $stackname = "$version-$stackname"
-    Write-Host "Stack name : $stackname"
-    #Set Variables
-    Write-Host "##vso[task.setvariable variable=stack;isOutput=true]$stackname"
-    Write-Host "##vso[task.setvariable variable=version;isOutput=true]$version"
-    Write-Host "##vso[task.setvariable variable=ImageID;isOutput=true]$amiID"
-    Write-Host "##vso[task.setvariable variable=IsEnabled;isOutput=true]True"
-    Write-host "The value of Variable IsEnabled is updated to True and output variable ImageID to $amiID"
+    try{
+        $amiID = Get-Content -Path $path
+        Write-Host "AMI ID $($amiID)"
+        $imageName = (Get-EC2Image -ImageId $amiID).Name
+        $imageName -match "$BaseImageName[-]?[0-9]+"
+        $version = $Matches[0]
+        Write-Host "Version : $version"
+        $stackname = "$version-$stackname"
+        Write-Host "Stack name : $stackname"
+        #Set Variables
+        Write-Host "##vso[task.setvariable variable=stack;isOutput=true]$stackname"
+        Write-Host "##vso[task.setvariable variable=version;isOutput=true]$version"
+        Write-Host "##vso[task.setvariable variable=ImageID;isOutput=true]$amiID"
+        Write-Host "##vso[task.setvariable variable=IsEnabled;isOutput=true]True"
+        Write-host "The value of Variable IsEnabled is updated to True and output variable ImageID to $amiID"
+    } catch{
+        $_ | Out-Default | Write-Host
+        Throw "Failed to set Task Variable"
+    }
 } else {
     Write-Host "Artifact path does NOT exist for $BaseImageName"
 }
