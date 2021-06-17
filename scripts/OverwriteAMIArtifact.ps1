1177293ff072366db6de17d124ca0eb07d4a3d91
Sharmil <skumar@gomoxie.com>
2021-06-17 12:25:56 +0530

    163405 Correcting the Sourcepath

--- scripts/OverwriteAMIArtifact.ps1
+++ scripts/OverwriteAMIArtifact.ps1
@@ -6,7 +6,7 @@ param (
 
 # OverwriteAMIArtifact.ps1 -BaseImageName w12r2d-14-2
 # Source file name is of the form copy-w12r2d-14-2.txt
-$sourcepath = "$($env:System_DefaultWorkingDirectory)/copy-$BaseImageName.txt"
+$sourcepath = "$($env:System_DefaultWorkingDirectory)/templates/support/scalable/ami-list/copy-$BaseImageName.txt"
 Write-Host "sourcepath = $sourcepath"
 
 $targetpath = "$($env:System_DefaultWorkingDirectory)/_Build Image Release Artefacts/aws-$BaseImageName/$BaseImageName.txt"
