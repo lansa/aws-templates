066d131bfa256cccc69afcdffb4661e0d17dd35a
priyadishah <priyanka.shah@accolitedigital.com>
2024-10-06 17:03:21 +0530

    wip

--- scripts/OverwriteAMIArtifact.ps1
+++ scripts/OverwriteAMIArtifact.ps1
@@ -1,9 +1,9 @@
 # OverwriteAMIArtifact.ps1 -BaseImageName w12r2d-14-2
 # Source file name is of the form copy-w12r2d-14-2.txt
-$sourcepath = "$($env:System_DefaultWorkingDirectory)/templates/support/scalable/ami-list/*.txt"
+$sourcepath = "cd "$($env:Pipeline_Workspace)/$($GitRepoName)"/templates/support/scalable/ami-list/*.txt"
 Write-Host "sourcepath = $sourcepath"
 
-$targetpath = "$($env:System_DefaultWorkingDirectory)/_Build Image Release Artefacts/aws"
+$targetpath = "cd "$($env:Pipeline_Workspace)/$($GitRepoName)"/_Build Image Release Artefacts/aws"
 Write-Host "targetpath = $targetpath"
 
 Remove-Item $targetpath/* -ErrorAction SilentlyContinue
