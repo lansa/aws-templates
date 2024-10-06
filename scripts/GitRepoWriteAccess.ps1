300655d40f73869cbce0fe0e79af27d60e66e5c7
priyadishah <priyanka.shah@accolitedigital.com>
2024-10-06 17:00:30 +0530

    wip

--- scripts/GitRepoWriteAccess.ps1
+++ scripts/GitRepoWriteAccess.ps1
@@ -25,7 +25,7 @@ param (
   )
 
 # goto git repo
-cd "$($env:System_DefaultWorkingDirectory)/$($GitRepoName)"
+cd "$($env:Pipeline_Workspace)/$($GitRepoName)"
 
 # git checkout to branch
 git checkout $GitBranch
