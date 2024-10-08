c8aa103c9f69ceb5cbaba1189c52d146e0b90d6e
hirendra-singh <hirendra.singh1@accolitedigital.com>
2024-10-08 20:34:57 +0530

    GitRepoWriteAccess.ps1
    Changed path ($env:System_DefaultWorkingDirectory to $($env:Pipeline_Workspace)

--- scripts/GitRepoWriteAccess.ps1
+++ scripts/GitRepoWriteAccess.ps1
@@ -25,7 +25,8 @@ param (
   )
 
 # goto git repo
-cd "$($env:System_DefaultWorkingDirectory)/$($GitRepoName)"
+#cd "$($env:System_DefaultWorkingDirectory)/$($GitRepoName)"
+cd "$($env:Pipeline_Workspace)/$($GitRepoName)" # Changed path ($env:System_DefaultWorkingDirectory to $($env:Pipeline_Workspace)
 
 # git checkout to branch
 git checkout $GitBranch
