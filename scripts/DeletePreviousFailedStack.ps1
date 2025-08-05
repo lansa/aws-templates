f0e74156e65d7316e43cad2f5e878da88388cbef
Rob Goodridge <goodridge.rob@gmail.com>
2025-08-06 09:04:41 +1000

    164362 Debug why stack deletion failed

--- scripts/DeletePreviousFailedStack.ps1
+++ scripts/DeletePreviousFailedStack.ps1
@@ -5,23 +5,23 @@ param (
 )
 
 try{
-Get-CFNStack -StackName $($Gatestack)
-Remove-CFNStack -StackName $($Gatestack) -Force -DeletionMode FORCE_DELETE_STACK
-$count =0
-while($count -lt 20){
-   try{
-         Get-CFNStack -StackName $($Gatestack)
-         Start-Sleep -Seconds 200
-         Write-Host "Deleting stack"
-         $count = $count +1
-       }
-    catch{
-         Write-Host "Stack Deleted"
-         break
-          }
-     }
+    Get-CFNStack -StackName $($Gatestack)
+    Remove-CFNStack -StackName $($Gatestack) -Force -DeletionMode FORCE_DELETE_STACK
+    $count =0
+    while($count -lt 20){
+        try{
+            Get-CFNStack -StackName $($Gatestack)
+            Start-Sleep -Seconds 200
+            Write-Host "Deleting stack"
+            $count = $count +1
+        } catch {
+            Write-Host "Stack Deleted"
+            break
+        }
+    }
     Write-Host "Stack deleted"
 } catch {
-    Write-Host "There is no stack or it was deleted"
+    $_.Exception.Message | Out-Default | Write-Host
+    Write-Host "Stack does not exist or is already deleted"
 }
 
